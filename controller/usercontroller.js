cc1d473c7604455b8092e125382ec6020f6428cd
Guptasatyambt <guptasatyamml@gmail.com>
2024-05-01 01:57:23 +0530

    30/04/242qq

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -53,7 +53,7 @@ async function handleregister(req,res){
         interest:interest,
     }}
     ,{new:true})
-    res.status(200).json({message:"Success",data:{email:email,name:user.name}});
+    res.status(200).json({message:"Success",data:{email:email}});
     }
 
     async function handlelogin(req,res){
