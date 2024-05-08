fa110aaea24d0c0a37e6e9b1af088018508cfb17
Guptasatyambt <guptasatyamml@gmail.com>
2024-05-08 19:55:28 +0530

    08/05/24

--- controller/feedbackcontroller.js
+++ controller/feedbackcontroller.js
@@ -6,11 +6,11 @@ async function handlefeedback(req, res) {
     const email = req.user.email;
     const { userFeedback } = req.body; // Changed the variable name to avoid conflict
      if (!email || !userFeedback) {
-        return res.status(400).json("Give us feedback");
+        return res.status(400).json({message:"Give Us Feedback"});
     }
    const user = await User.findOne({ email });
         if (!user) {
-            return res.status(404).json("User not found");
+            return res.status(404).json({message:"User Not Found"});
         }
         const _id = user._id;
         const feed = await Feedback.create({
@@ -19,10 +19,10 @@ async function handlefeedback(req, res) {
             feedback: userFeedback // Assuming userFeedback is the feedback content
         });
 
-        return res.status(200).json("Thanks for feedback");
+        return res.status(200).json({message:"Success"});
     } catch (error) {
         console.error(error);
-        return res.status(500).json("Internal Server Error");
+        return res.status(500).json({message:"Internal Server Error"});
     }
 }
 
