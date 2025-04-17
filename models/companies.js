abf112d94dd1f46eb05a6ffa9a33353709ccab69
Guptasatyambt <guptasatyamml@gmail.com>
2025-04-17 15:54:02 +0530

    changes in user,company model

--- models/companies.js
+++ models/companies.js
@@ -9,53 +9,6 @@ const companySchema = new mongoose.Schema({
         type: String,
         require: false,
     },
-    position: {
-        type: String,
-        require: false,
-    },
-    package_lpa: {
-        type: Number,
-        require: false,
-    },
-    students_hired: {
-        type: Number,
-        default: 0,
-    },
-    hired_students: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
-    }],
-    job_description: {
-        type: String,
-        require: false,
-    },
-    visit_date: {
-        type: Date,
-        require: true,
-    },
-    application_deadline: {
-        type: Date,
-        require: true,
-    },
-    eligibility_criteria: {
-        min_cgpa: {
-            type: Number,
-            require: true,
-        },
-        allowed_branches: [{
-            type: String,
-            require: true,
-        }],
-        allowed_batch_year: {
-            type: String,
-            require: true,
-        }
-    },
-    status: {
-        type: String,
-        enum: ['upcoming', 'ongoing', 'completed'],
-        default: 'upcoming'
-    }
 }, { timestamps: true });
 
 const COMPANY = mongoose.model('company', companySchema);
